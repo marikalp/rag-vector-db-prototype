ffb97d1584f65c772d1a9203fe4e941a028f7e22
marikalp <lapismarik@gmail.com>
2026-02-28 20:36:13 +0100

    Add type hints to BaseVectorStore methods

--- src/vectorstores/base_vectorstore.py
+++ src/vectorstores/base_vectorstore.py
@@ -1,12 +1,13 @@
+from typing import List, Dict, Any, Optional
+
 class BaseVectorStore:
     """Abstract interface for vector database backends."""
 
-    def create_index(self):
+    def create_index(self) -> None:
         raise NotImplementedError
 
-    def insert(self, vectors, metadata=None):
+    def insert(self, vectors: List[List[float]], metadata: Optional[List[Dict[str, Any]]] = None) -> None:
         raise NotImplementedError
 
-    def query(self, vector, k=5, filters=None):
+    def query(self, vector: List[float], k: int = 5, filters: Optional[Dict[str, Any]] = None) -> List[Dict[str, Any]]:
         raise NotImplementedError
-
