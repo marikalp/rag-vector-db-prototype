89c80d709bba674db7efe0226f5521d91322738d
marikalp <lapismarik@gmail.com>
2026-02-28 20:10:42 +0100

    Add abstract BaseVectorStore class for vector databases

--- src/vectorstores/base_vectorstore.py
+++ src/vectorstores/base_vectorstore.py
@@ -1,1 +1,12 @@
+class BaseVectorStore:
+    """Abstract interface for vector database backends."""
+
+    def create_index(self):
+        raise NotImplementedError
+
+    def insert(self, vectors, metadata=None):
+        raise NotImplementedError
+
+    def query(self, vector, k=5, filters=None):
+        raise NotImplementedError
 
